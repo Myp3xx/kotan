87627dc5d704f155482b9aefda10276104c6d40c
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-01-09 22:15:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json");
 
 client.on('ready', () => {
     client.user.setPresence({ game: { name: 'k.help', type: 0 } });
 });
 
 client.on("message", (message) => {
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+if(command === "say") {
+                const sayMessage = args.join(" ");
+                message.delete().catch(O_o=>{});  
+                message.channel.send(sayMessage);
+        }
 if(message.content == "k.help"){
 		message.channel.send("> k.ping - Проверка\n> k.serv - Наш сервер\n> k.mc - Халявная лицензия Minecraft\n> *Бот в разработке*");
 	}
