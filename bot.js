718dd32ae5ab51dedf719d5624d31bc77ca16f39
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-01-09 20:17:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: 'ЧСВ', type: 0 }
+    client.user.setStatus('dnd')
+    client.user.setPresence({ game: { name: 'ванной с уточками', type: 0 }
 });
 
 client.on('message', message => {
-    if (message.content === 'Кто ты?') {
-    	message.reply('Я бот');
+    if (message.content === 'k.info') {
+    	message.reply('ГЫГ');
   	}
 });
 
