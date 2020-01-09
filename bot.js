686dbe67b25529f56e15f3f454f1c3b1b3b598d3
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-01-09 21:26:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,16 @@ client.on('ready', () => {
 
 client.on("message", (message) => {
 if(message.content == "k.help"){
-		message.channel.send("> k.ping - Проверка\n> k.mc - Халявная лицензия Minecraft\n> *Бот в разработке*");
+		message.channel.send("> k.ping - Проверка\n> k.serv - Наш сервер\n> k.mc - Халявная лицензия Minecraft\n> *Бот в разработке*");
 	}
 if(message.content == "k.ping"){
 		message.channel.send("> 🐾");
 	}
+if(message.content == "k.serv"){
+		message.author.send("https://discord.gg/qS7nza3");
+	}
 if(message.content == "k.mc"){
-		message.author.send("https://discord.gg/hMVQxwv");
+		message.author.send("> Держи свой аккаунт:\n> ``braxtonian97@gmail.com:bmbbgk06``\n> (логин:пароль)\n> **Хочешь еще лицензию? Тогда тебе сюда - https://discord.gg/hMVQxwv**");
 	}
 });
 
