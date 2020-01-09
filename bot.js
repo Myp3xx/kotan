5f538846a47744ced5afd6b4fef997ba236fa070
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-01-09 19:41:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: 'Myp3uk' }, status: 'idle' })
-    console.log(`${client.user.username} is up and running!`);
+    client.user.setPresence({
+        game: { 
+            name: 'test',
+            type: 'WATCHING'
+        },
+        status: 'dnd'
     })
 });
 
