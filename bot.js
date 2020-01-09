b62f65637d776f11e5e24e3aff9423e5856996bd
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-01-09 19:47:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('chevo')
+    bot.user.setStatus('dnd')
+    bot.user.setPresence({
+        game: {
+            name: 'with depression',
+            type: "STREAMING",
+            url: "https://www.twitch.tv/monstercat"
+        }
+    });
 });
 
 client.on('message', message => {
