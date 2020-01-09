0c9f1a3ed1a0fd881126ef24f31c6fdbcd9a6a8d
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-01-09 19:16:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,16 +5,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('ready', () => {
-    client.user.setStatus('available')
-    client.user.setPresence({
-        game: {
-            name: 'пиздец',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
-        }
-});
-
 client.on('message', message => {
     if (message.content === 'Кто ты?') {
     	message.reply('Я бот');
