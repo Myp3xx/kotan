e50b38dc514fd1b1b6aa04fdec16563b901cd22a
Myp3ukkk <58299231+Myp3ukkk@users.noreply.github.com>
2020-01-10 11:58:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: 'кошки-мышки', type: 0 } });
+    client.user.setPresence({ game: { name: 'пиздец', type: 1 } });
 });
 
 client.on("message", (message) => {
